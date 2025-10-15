850ad0e7415c4cde6acac6a913c9bab62563ec97
Smahdi2005 <samieimahdi685@gmail.com>
2025-10-15 18:19:58 +0330

    empty version

---
+++ Data_Analyse_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Data_Analyse_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
